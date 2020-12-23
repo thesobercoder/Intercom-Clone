db9c78df87981eb0752d5d6a5fc74918c0bc0e04
Soham Dasgupta <soham1.dasgupta@gmail.com>
2020-12-23 19:21:03 +0530

    Finished chat reply area

--- pages/index.js
+++ pages/index.js
@@ -203,7 +203,98 @@ const ChatPane = (props) => {
         </div>
       </div>
       <div className="flex-grow">Chat holder</div>
-      <div className="flex-none h-1/5 border-t border-gray-300">Reply Area</div>
+      <div className="flex-none h-1/5 border-t text-gray-400 border-gray-300 p-4 flex flex-col">
+        <div className="flex flex-row space-x-8  pb-2 flex-none items-center">
+          <button className="border-b-2 border-gray-400 pb-2">Reply</button>
+          <button className="pb-2 border-b-2 border-transparent">Note</button>
+        </div>
+        <textarea
+          className="flex-grow focus:outline-none"
+          placeholder="Type # to add a saved reply or try sending an article"
+        ></textarea>
+        <div className="h-1/5 flex-none flex flex-row space-x-4 items-center">
+          <button className="focus:outline-none">
+            <svg
+              className="h-4 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+              />
+            </svg>
+          </button>
+          <button className="focus:outline-none">
+            <svg
+              className="h-4 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+              />
+            </svg>
+          </button>
+          <button className="focus:outline-none">
+            <svg
+              className="h-4 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+          <button className="focus:outline-none">
+            <svg
+              className="h-4 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+              />
+            </svg>
+          </button>
+          <button className="focus:outline-none">
+            <svg
+              className="h-4 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
